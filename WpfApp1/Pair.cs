fcd68782dc7d1763fd32c923e79f01f3aa38aa1e
исп-31 <-31@LAB44-13>
2022-12-07 11:39:17 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Pair.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Pair
+    {
+        private int _first;
+        private int _second;
+
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+
+        public int First
+        {
+            get { return _first; }
+            set { _first = value; }
+        }
+
+        public int Second
+        {
+            get { return _second; }
+            set { _second = value; }
+        }
+
+
+        public Pair Substraction(Pair pair)
+        {
+            return new Pair(First - pair.First, Second - pair.Second);
+        }
+        public Pair Substraction(Pair pair, Pair pair1)
+        {
+            return new Pair(First - pair.First - pair1.First, Second - pair.Second - pair1.Second);
+        }
+        public static bool operator true(Pair pair)
+        {
+            if (pair.First == pair.Second && pair.First == pair.Second)
+            {
+                return false;
+            }
+            else return true;
+        }
+        public static bool operator false(Pair pair)
+        {
+            return pair.First != pair.Second;
+        }
+    }
+}
